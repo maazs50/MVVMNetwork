7b3e2ed5735aace1c8276fef4804c397ffef4234
Maaz <maazs732@gmail.com>
2023-07-29 22:38:22 +0530

    creating and binding viewHolders for single view type done

--- app/src/main/java/com/example/mvvmnetwork/ui/NewsListAdapter.kt
+++ app/src/main/java/com/example/mvvmnetwork/ui/NewsListAdapter.kt
@@ -3,9 +3,11 @@ package com.example.mvvmnetwork.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mvvmnetwork.R
 import com.example.mvvmnetwork.databinding.ListItemBinding
 import com.example.mvvmnetwork.extenstions.loadImage
 import com.mvvmnews.api.models.Article
@@ -13,20 +15,22 @@ import com.mvvmnews.api.models.Article
 class NewsListAdapter() : ListAdapter<Article,NewsListAdapter.NewsViewHolder>
     (NewsDiffUtilCallback()) {
 
-    inner class NewsViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview)
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val binding = ListItemBinding.inflate(
-            parent.context.getSystemService(LayoutInflater::class.java),
-            parent,
-            false
-        )
-        return NewsViewHolder(binding.root)
+       return NewsViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
        val article = getItem(position)
-        ListItemBinding.bind(holder.itemView).apply {
+        holder.bind(article, onItemClickListener)
+    }
+
+//ViewHolder
+class NewsViewHolder private constructor(itemview: View) : RecyclerView.ViewHolder(itemview){
+    //Binding data
+    fun bind(article: Article, onItemClickListener: ((Article)->Unit)?){
+        ListItemBinding.bind(itemView).apply {
             tvSource.text = article.source.name
             tvTitle.text = article.title
             tvDescription.text = article.description
@@ -38,9 +42,20 @@ class NewsListAdapter() : ListAdapter<Article,NewsListAdapter.NewsViewHolder>
                 }
             }
         }
-
     }
-
+    //Creating a viewHolder
+    companion object{
+        fun from(parent: ViewGroup): NewsViewHolder{
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ListItemBinding.inflate(
+                layoutInflater,
+                parent,
+                false
+            )
+            return NewsViewHolder(binding.root)
+        }
+    }
+}
 
     class NewsDiffUtilCallback: DiffUtil.ItemCallback<Article>(){
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
